13bb9749c978c47915c7c1f3110f37ab0efebdba
AntonVitulskyi <vitulskiyanton@gmail.com>
2023-01-09 19:00:47 +0200

    Wrote on hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,32 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Statistics from '../Statistics/Statistics';
 import FeedbackOptions from '../Feedback/FeedbackOptions';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 
-export default class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const Feedback = () => {
+   const [good, setGood] = useState(0)
+   const [neutral, setNeutral] = useState(0)
+   const [bad, setBad] = useState(0)
+ 
+
+ const onClickChangeValue = (event) => {
+switch (event.target.name) {
+  case "good":
+    setGood(good + 1);
+    break;
+    case "neutral":
+      setNeutral(neutral + 1)
+    break;
+    case "bad":
+      setBad(bad + 1)
+    break;
+  default:
+    break;
+}
   };
 
-  onClickChangeValue = event => {
-    this.setState(prevState => {
-      return { [event.target.name]: prevState[event.target.name] + 1 };
-    });
-  };
-  render() {
-    let { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
     const positivePercentage = Math.round((good / total) * 100) || 0;
+
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onClickChangeValue}
+            options={["good","neutral", "bad" ]}
+            onLeaveFeedback={onClickChangeValue}
           />
         </Section>
         <Section title="Statistics">
@@ -34,9 +43,9 @@ export default class Feedback extends Component {
             <Notification message="There is no feedback" />
           ) : (
             <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
+              good={good}
+              neutral={neutral}
+              bad={bad}
               total={total}
               positivePercentage={positivePercentage}
             />
@@ -45,4 +54,5 @@ export default class Feedback extends Component {
       </>
     );
   }
-}
+
+export default Feedback;
